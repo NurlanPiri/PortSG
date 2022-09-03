185d82ce9f6e4694d56ff16e18674ecf3be62acd
[NurlanPiri] <[nrlnprv@gmail.com]>
2022-09-03 22:41:55 +0400

    first commit

---
+++ PortSG/PortSG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortSG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
